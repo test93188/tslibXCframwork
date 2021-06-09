33c19b7094b33ff21d6c90d1a99b2886746f8553
Parveen <parveenk@appinfoinc.com>
2021-06-09 18:33:36 +0530

    commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MyLibrary",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MyLibrary",
+            targets: ["MyLibrary", "SomeRemoteBinaryPackage", "SomeLocalBinaryPackage"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MyLibrary",
+            exclude: ["instructions.md"],
+            resources: [
+                       ]
+        ),
+        .binaryTarget(
+            name: "SomeRemoteBinaryPackage",
+            url: "https://github.com/test93188/tslibXCframwork.git",
+            checksum: "https://github.com/test93188/tslibXCframwork.git"
+        ),
+    ]
+)
