eecdbd55feb1e7a2b7235fc7652901e535e28a76
Parveen <parveenk@appinfoinc.com>
2021-06-09 18:37:41 +0530

    commit

--- Package.swift
+++ Package.swift
@@ -1,33 +1,24 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "SwiftLeePackage",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        // Only add support for iOS 11 and up.
+        .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary", "SomeRemoteBinaryPackage", "SomeLocalBinaryPackage"])
+        .library(name: "SwiftLeePackage", targets: ["SwiftLeePackage"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        /// Define the Mocker dependency:
+        .package(url: "https://github.com/test93188/tslibXCframwork.git", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary",
-            exclude: ["instructions.md"],
-            resources: [
-                       ]
-        ),
-        .binaryTarget(
-            name: "SomeRemoteBinaryPackage",
-            url: "https://github.com/test93188/tslibXCframwork.git",
-            checksum: "https://github.com/test93188/tslibXCframwork.git"
-        ),
+        .target(name: "SwiftLeePackage", dependencies: []),
+        /// Add it to your test target in the dependencies array:
+        .testTarget(name: "SwiftLeePackageTests", dependencies: ["SwiftLeePackage", "Mocker"])
     ]
 )
