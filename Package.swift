eb015a745de3431b64ff65204270cd756653250f
Applied Information <appinfoindia@gmail.com>
2021-06-10 12:00:12 +0530

    first

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TravelSafelyBoneshell",
+    platforms: [
+        // Only add support for iOS 11 and up.
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "TravelSafelyBoneshell", targets: ["TravelSafelyBoneshell"])
+    ],
+    dependencies: [
+        /// Define the Mocker dependency:
+        .package(url: "https://github.com/test93188/tslibXCframwork.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "testProject", dependencies: []),
+        /// Add it to your test target in the dependencies array:
+        .testTarget(name: "testProject", dependencies: ["TravelSafelyBoneshell"])
+    ]
+)
---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Parveen Kumar on 10/06/21.
+//  Copyright © 2021 Ai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
